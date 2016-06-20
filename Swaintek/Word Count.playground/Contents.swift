b936b0e6d4e26b8d9c40e5e01eff45362fb07cfc
David Swaintek <davidswaintek@Davids-MacBook-Pro.local>
2016-06-20 16:55:17 -0700

    Week 3 class 1 homework

---
+++ Swaintek/Word Count.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "One two three four five, six seven eight Nine ten. Eleven?"
+
+func wordCount(s: String) -> Int
+{
+    let words = s.componentsSeparatedByString(" ")
+    print(words)
+    return words.count
+}
+
+wordCount(str)
