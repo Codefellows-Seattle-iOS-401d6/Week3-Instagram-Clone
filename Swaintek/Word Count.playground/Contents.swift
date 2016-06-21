c68079e1ccec4e215f8c740c0a15e56218822307
David Swaintek <davidswaintek@Davids-MacBook-Pro.local>
2016-06-21 13:58:24 -0700

    Add .gitignore

--- Swaintek/Word Count.playground/Contents.swift
+++ Swaintek/Word Count.playground/Contents.swift
@@ -6,9 +6,10 @@ var str = "One two three four five, six seven eight Nine ten. Eleven?"
 
 func wordCount(s: String) -> Int
 {
-    let words = s.componentsSeparatedByString(" ")
+    let words = s.componentsSeparatedByString(NSRegularExpression(\b))
     print(words)
     return words.count
 }
 
 wordCount(str)
+
