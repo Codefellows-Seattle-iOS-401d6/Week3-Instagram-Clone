199ecb723627608760b66fd7da478065ebce471a
David Swaintek <davidswaintek@Davids-MacBook-Pro.local>
2016-06-21 14:17:41 -0700

    Attempted code challenge with regex, still not working, submitting anyway

--- Swaintek/Word Count.playground/Contents.swift
+++ Swaintek/Word Count.playground/Contents.swift
@@ -2,14 +2,44 @@
 
 import UIKit
 
-var str = "One two three four five, six seven eight Nine ten. Eleven?"
 
 func wordCount(s: String) -> Int
 {
-    let words = s.componentsSeparatedByString(NSRegularExpression(\b))
-    print(words)
-    return words.count
+    do {
+        let regex = try NSRegularExpression(pattern: "[a-zA-Z]", options: .CaseInsensitive)
+        let range = NSRange(location: 0, length: s.characters.count)
+        let results = [NSTextCheckingResult] = regex.matchesInString(s, options: .Anchored, range: range)
+        
+        return results.count
+    }
+
+    catch {
+        print(error)
 }
 
-wordCount(str)
+wordCount("How many words is this")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var str = "One two three four five, six seven eight Nine ten. Eleven?"
+//
+//func wordCount(s: String) -> Int
+//{
+//    let words = s.componentsSeparatedByString(NSRegularExpression(\b))
+//    print(words)
+//    return words.count
+//}
+//
+//wordCount(str)
 
